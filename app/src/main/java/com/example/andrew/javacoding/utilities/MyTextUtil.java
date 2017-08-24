7f374a4b5b74714d47b70c8408fd714d091adb35
Joshua He <97850500@qq.com>
2017-08-24 09:05:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/andrew/javacoding/utilities/MyTextUtil.java
@@ -0,0 +1,12 @@
+package com.example.andrew.javacoding.utilities;
+
+/**
+ * Created by Andrew on 2017/8/23.
+ */
+public class MyTextUtil {
+
+    public String getValue() {
+        return "Hello from the utility class";
+    }
+
+}
